1cb470fbdf8c9430195222f84181285907e4d29d
Russell McCreath <russell.mccreath@phs.scot>
2021-08-26 16:58:36 +0100

    build r pathway page

--- pages/develop/r-pathway.vue
+++ pages/develop/r-pathway.vue
@@ -4,7 +4,8 @@
       <b-card no-body class="overflow-hidden head-card">
         <b-row no-gutters>
           <b-col sm="12" md="9">
-            <b-card-body title="R Pathway">
+            <b-card-body>
+              <h3>R Pathway</h3>
               <b-card-text>
                 PHS has made efforts to build an infrastructure for R. Here, the pathway for learning and development is laid out, including training and guidance.
               </b-card-text>
@@ -17,23 +18,25 @@
     <div>
       <b-row cols="1" cols-md="2">
         <b-col cols="12" lg="6">
-          <h5>What is R?</h5>
+          <h4>What is R?</h4>
           <b-row>
             <b-col>
               <p>This video is an overview of what R is and what it can do. Without getting into technical details, the video has a focus on how to get started and build your skills within PHS.</p>
+              <p>This page has all resources related to R. See below for the training pathway, this is split into 3 sections (each link will take you directly to the section on this page): </p>
+              <b-row class="mb-3">
+                <b-col class="text-center grid" align-self='center'>
+                  <b-button variant="outline-primary" @click="$router.push('#get_started')">Get Started</b-button>
+                </b-col>
+                <b-col class="text-center grid" align-self='center'>
+                  <b-button variant="outline-primary" @click="$router.push('#take_it_further')">Take it Further</b-button>
+                </b-col>
+                <b-col class="text-center grid" align-self='center'>
+                  <b-button variant="outline-primary" @click="$router.push('#get_advanced')">Get Advanced</b-button>
+                </b-col>
+              </b-row>
             </b-col>
           </b-row>
-          <b-row class="mb-3">
-            <b-col class="text-center grid" align-self='center'>
-              <b-button variant="outline-primary" @click="$router.push('#get_started')">Get Started</b-button>
-            </b-col>
-            <b-col class="text-center grid" align-self='center'>
-              <b-button variant="outline-primary" @click="$router.push('#take_it_further')">Take it Further</b-button>
-            </b-col>
-            <b-col class="text-center grid" align-self='center'>
-              <b-button variant="outline-primary" @click="$router.push('#get_advanced')">Get Advanced</b-button>
-            </b-col>
-          </b-row>
+
         </b-col>
         <b-col cols="12" lg="6">
             <b-embed
@@ -53,39 +56,54 @@
                 <a id="get_started"></a>
                 <h4 class="mt-5">Get Started</h4>
                 <b-row cols="1" cols-md="2" class="pr-md-0 pr-lg-5">
-                  <b-col class="grid">
-                    <b-card
-                    title="Introduction to R"
-                    sub-title="Online"
-                    tag="article"
-                    class="mb-4">
-                      <b-card-text>
-                        A beginner's course for learning R from the basics.
-                      </b-card-text>
-                      <template v-slot:footer>
-                        <b-button variant="primary">
-                          Start
-                        </b-button>
-                      </template>
-                    </b-card>
+
+                  <b-col
+                  class="grid"
+                  v-for='course in courses.filter( course => course.technology == "R" && course.level == "1" && course.link != "")'
+                  :key="course.title">
+                    <b-card-group deck>
+                      <b-card
+                      class="mb-4">
+                        <b-card-title>
+                          <NuxtLink :to="'/develop/' + course.slug" class="course-link">{{course.title}}</NuxtLink>
+                        </b-card-title>
+                        <b-card-text>
+                          {{course.description}}
+                        </b-card-text>
+
+                        <template v-slot:footer>
+
+                          <b-list-group flush>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.f2f != ''">
+                              In-Person Course
+                              <b-button
+                                v-b-modal.booking-modal
+                                variant="primary"
+                                @click="selectedCourse = course">
+                                Book
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.online != ''">
+                              Online Course
+                              <b-button
+                                :href="course.online"
+                                target="_blank"
+                                variant="primary">
+                                Start
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="coming-soon ml-auto" v-if="course.f2f == '' && course.online == ''">
+                              <i>Coming Soon</i>
+                            </b-list-group-item>
+                          </b-list-group>
+
+                        </template>
+
+                      </b-card>
+                  </b-card-group>
+
                   </b-col>
 
-                  <b-col>
-                    <b-card
-                    title="Introduction to R"
-                    sub-title="In-Person"
-                    tag="article"
-                    class="mb-4">
-                      <b-card-text>
-                        A beginner's course for learning R from the basics.
-                      </b-card-text>
-                      <template v-slot:footer>
-                        <b-button variant="primary">
-                          Book
-                        </b-button>
-                      </template>
-                    </b-card>
-                  </b-col>
                 </b-row>
               </b-col>
             </b-row>
@@ -95,42 +113,54 @@
                 <h4 class="mt-5">Take it Further</h4>
 
                 <b-row cols="1" cols-md="2" class="pr-md-0 pr-lg-5">
-                  <b-col class="grid">
-                    <b-card
-                    title="R Markdown"
-                    sub-title="Online"
-                    tag="article"
-                    class="mb-4">
-                      <b-card-text>
-                        Learn about building reports and other functionality of R Markdown.
-                      </b-card-text>
-                      <template v-slot:footer>
-                        <b-button variant="primary">
-                          Start
-                        </b-button>
-                      </template>
-                    </b-card>
-                  </b-col>
 
-                  <b-col>
-                    <b-card
-                    title="Data Visualisation in R"
-                    sub-title="Online"
-                    tag="article"
-                    class="mb-4">
-                      <b-card-text>
-                        Learn about data viz using R. This course has a focus on ggplot2 but includes an introduction to plotly.
-                      </b-card-text>
-                      <template v-slot:footer>
-                        <b-button variant="primary">
-                          Start
-                        </b-button>
-                      </template>
-                    </b-card>
+                  <b-col
+                  class="grid"
+                  v-for='course in courses.filter( course => course.technology == "R" && course.level == "2" && course.link != "")'
+                  :key="course.title">
+                    <b-card-group deck>
+                      <b-card
+                      class="mb-4">
+                        <b-card-title>
+                          <NuxtLink :to="'/develop/' + course.slug" class="course-link">{{course.title}}</NuxtLink>
+                        </b-card-title>
+                        <b-card-text>
+                          {{course.description}}
+                        </b-card-text>
+
+                        <template v-slot:footer>
+
+                          <b-list-group flush>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.f2f != ''">
+                              In-Person Course
+                              <b-button
+                                v-b-modal.booking-modal
+                                variant="primary"
+                                @click="selectedCourse = course">
+                                Book
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.online != ''">
+                              Online Course
+                              <b-button
+                                :href="course.online"
+                                target="_blank"
+                                variant="primary">
+                                Start
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="coming-soon ml-auto" v-if="course.f2f == '' && course.online == ''">
+                              <i>Coming Soon</i>
+                            </b-list-group-item>
+                          </b-list-group>
+
+                        </template>
+
+                      </b-card>
+                  </b-card-group>
+
                   </b-col>
                 </b-row>
-
-
               </b-col>
             </b-row>
 
@@ -140,19 +170,67 @@
                 <h4 class="mt-5">Get Advanced</h4>
 
                 <b-row cols="1" cols-md="2" class="pr-md-0 pr-lg-5">
+
+                  <b-col
+                  class="grid"
+                  v-for='course in courses.filter( course => course.technology == "R" && course.level == "3" && course.link != "")'
+                  :key="course.title">
+                    <b-card-group deck>
+                      <b-card
+                      class="mb-4">
+                        <b-card-title>
+                          <NuxtLink :to="'/develop/' + course.slug" class="course-link">{{course.title}}</NuxtLink>
+                        </b-card-title>
+                        <b-card-text>
+                          {{course.description}}
+                        </b-card-text>
+
+                        <template v-slot:footer>
+
+                          <b-list-group flush>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.f2f != ''">
+                              In-Person Course
+                              <b-button
+                                v-b-modal.booking-modal
+                                variant="primary"
+                                @click="selectedCourse = course">
+                                Book
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.online != ''">
+                              Online Course
+                              <b-button
+                                :href="course.online"
+                                target="_blank"
+                                variant="primary">
+                                Start
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="coming-soon ml-auto" v-if="course.f2f == '' && course.online == ''">
+                              <i>Coming Soon</i>
+                            </b-list-group-item>
+                          </b-list-group>
+
+                        </template>
+
+                      </b-card>
+                  </b-card-group>
+
+                  </b-col>
+
                   <b-col class="grid">
                     <b-card
                     title="R Shiny"
-                    sub-title="In-Person"
-                    tag="article"
                     class="mb-4">
                       <b-card-text>
                         Learn how to develop web apps and dashboards using R Shiny.
                       </b-card-text>
                       <template v-slot:footer>
-                        <b-button variant="primary">
-                          Book
-                        </b-button>
+                        <b-list-group flush>
+                          <b-list-group-item class="coming-soon ml-auto">
+                            <i>Coming Soon</i>
+                          </b-list-group-item>
+                        </b-list-group>
                       </template>
                     </b-card>
                   </b-col>
@@ -166,21 +244,51 @@
                 <h4 class="mt-5">Related Technologies</h4>
 
                 <b-row cols="1" cols-md="2" class="pr-md-0 pr-lg-5">
-                  <b-col class="grid">
-                    <b-card
-                    title="Introduction to Git"
-                    sub-title="In-Person Course"
-                    tag="article"
-                    class="mb-4">
-                      <b-card-text>
-                        Learn how to develop web apps and dashboards using R Shiny.
-                      </b-card-text>
-                      <template v-slot:footer>
-                        <b-button variant="primary">
-                          Book
-                        </b-button>
-                      </template>
-                    </b-card>
+                  <b-col
+                  class="grid"
+                  v-for="course in courses.filter( course => course.related.includes('R'))">
+                    <b-card-group deck>
+                      <b-card class="mb-4">
+                        <b-card-title>
+                          <NuxtLink :to="'/develop/' + course.slug" class="course-link">
+                            {{course.title}}
+                          </NuxtLink>
+                        </b-card-title>
+                        <b-card-text>
+                          {{course.description}}
+                        </b-card-text>
+
+                        <template v-slot:footer>
+
+                          <b-list-group flush>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.f2f != ''">
+                              In-Person Course
+                              <b-button
+                                v-b-modal.booking-modal
+                                variant="primary"
+                                @click="selectedCourse = course">
+                                Book
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="d-flex justify-content-between align-items-center" v-if="course.online != ''">
+                              Online Course
+                              <b-button
+                                :href="course.online"
+                                target="_blank"
+                                variant="primary">
+                                Start
+                              </b-button>
+                            </b-list-group-item>
+                            <b-list-group-item class="coming-soon ml-auto" v-if="course.f2f == '' && course.online == ''">
+                              <i>Coming Soon</i>
+                            </b-list-group-item>
+                          </b-list-group>
+
+                        </template>
+
+                      </b-card>
+                    </b-card-group>
+
                   </b-col>
                 </b-row>
 
@@ -191,12 +299,25 @@
 
           <b-col cols="12" lg="3">
             <b-row>
-              <b-col>
-                <h4 class="mt-5">Guidance</h4>
-
+              <b-col
+                class="grid"
+                v-for="resource in resources.filter(resource => resource.technology == 'R')"
+                :key="resource.title">
+                <h4 class="mt-5">Other Resources</h4>
                 <b-list-group>
-                  <b-list-group-item href="#">R Style Guide<eva-icon class="float-right" name="file-text-outline" fill="#bd27b9"></eva-icon></b-list-group-item>
-                  <b-list-group-item href="#">R Textbook<eva-icon class="float-right" name="book-outline" fill="#bd27b9"></eva-icon></b-list-group-item>
+                  <b-list-group-item
+                    v-if="resource.type == 'Guidance'"
+                    v-b-modal.guidance-modal
+                    @click="showGuidance(resource)">
+                    {{resource.title}}
+                    <eva-icon class="float-right" name="file-text-outline" fill="#bd27b9"></eva-icon>
+                  </b-list-group-item>
+                  <b-list-group-item
+                    v-else-if="resource.type == 'Textbook'"
+                    :href="resource.link">
+                    {{resource.title}}
+                    <eva-icon class="float-right" name="book-outline" fill="#bd27b9"></eva-icon>
+                  </b-list-group-item>
                 </b-list-group>
 
               </b-col>
@@ -207,10 +328,52 @@
       </div>
 
     </div>
+
+    <b-modal id="guidance-modal" size="xl" scrollable :title="selectedResource.title">
+      If this content doesn't load, <a target="_blank" :href="selectedResource.richLink">click here</a> to open in a browser tab.
+      </br>
+    </br>
+      <vue-markdown class="guidance-md" :source="selectedResource.md"></vue-markdown>
+    </b-modal>
+
+    <b-modal id="booking-modal" size="xl">
+      If this content doesn't load, <a target="_blank" :href="selectedCourse.f2f">click here</a> to open in a browser tab.
+      </br>
+      <div v-if="selectedCourse.f2f">
+        <iframe width="640px" height= "1550px" :src= "selectedCourse.f2f" frameborder= "0" marginwidth= "0" marginheight= "0" style= "border: none; width:100%" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+      </div>
+    </b-modal>
+
   </div>
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+import courses from '@/assets/courses.json'
+import resources from '@/assets/resources.json'
+
+export default {
+  components: { VueMarkdown },
+  data(){
+    return {
+      courses,
+      resources,
+      selectedCourse: {},
+      selectedResource: {}
+    }
+  },
+  computed:{
+    pathwayResources(){
+      let resources = this.resources.filter(resource => resource.technology == "R")
+    }
+  },
+  methods:{
+    async showGuidance(resource){
+      resource.md = await this.$axios.$get('https://secret-ocean-49799.herokuapp.com/'+resource.link)
+      this.selectedResource=resource
+    }
+  }
+}
 </script>
 
 <style>
