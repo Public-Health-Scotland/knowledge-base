9e855c732a8cace4b1524ea67069ab4560bf7241
Russell McCreath <russell.mccreath@phs.scot>
2021-08-27 17:37:23 +0100

    add feedback function

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,108 +1,118 @@
 <template>
   <div>
 
-    <head>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3D55TZR6QM"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+        <head>
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-3D55TZR6QM"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-        gtag('config', 'G-3D55TZR6QM');
-      </script>
-      <script src="https://cdn.lordicon.com/libs/frhvbuzj/lord-icon-2.0.2.js"></script>
-    </head>
+            gtag('config', 'G-3D55TZR6QM');
+          </script>
+          <script src="https://cdn.lordicon.com/libs/frhvbuzj/lord-icon-2.0.2.js"></script>
+        </head>
 
-    <div class="header-container">
-      <header>
-        <div class="container">
-          <div class="d-flex justify-content-between align-items-center">
-            <div class="left">
-              <img
-                class="logo cursor-pointer"
-                src="~/static/phs-logo.png"
-                alt="PHS Logo"
-                @click="$router.push('/')">
+        <div class="header-container">
+          <header>
+            <div class="container">
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="left">
+                  <img
+                    class="logo cursor-pointer"
+                    src="~/static/phs-logo.png"
+                    alt="PHS Logo"
+                    @click="$router.push('/')">
+                </div>
+              </div>
             </div>
-          </div>
+          </header>
         </div>
-      </header>
-    </div>
 
-    <div class="nav-container">
-      <b-navbar toggleable="sm" type="dark">
-        <div class="container">
-          <b-navbar-brand
-            class="cursor-pointer"
-            @click="$router.push('/')">
-              Data Science Knowledge Base
-            </b-navbar-brand>
+        <div class="nav-container">
+          <b-navbar toggleable="sm" type="dark">
+            <div class="container">
+              <b-navbar-brand
+                class="cursor-pointer"
+                @click="$router.push('/')">
+                  Data Science Knowledge Base
+                </b-navbar-brand>
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+              <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav class="ml-auto">
+              <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav class="ml-auto">
 
-              <NuxtLink to="/develop" tag="b-nav-item">Develop</NuxtLink>
-              <NuxtLink to="/social" tag="b-nav-item">Social</NuxtLink>
+                  <NuxtLink to="/develop" tag="b-nav-item">Develop</NuxtLink>
+                  <NuxtLink to="/social" tag="b-nav-item">Social</NuxtLink>
 
-              <!-- <b-nav-item href=""><strong>Develop</strong></b-nav-item>
-              <b-nav-item href=""><strong>Social</strong></b-nav-item> -->
-              <b-nav-item v-b-modal.help-modal><eva-icon name="question-mark-circle-outline" fill="#fff"></eva-icon></b-nav-item>
-              <b-modal id="help-modal" size="lg" title="Help">
+                  <!-- <b-nav-item href=""><strong>Develop</strong></b-nav-item>
+                  <b-nav-item href=""><strong>Social</strong></b-nav-item> -->
+                  <b-nav-item v-b-modal.help-modal><eva-icon name="question-mark-circle-outline" fill="#fff"></eva-icon></b-nav-item>
+                  <b-modal id="help-modal" size="lg" title="Help">
 
-                <p class="my-4"><strong>Training Request</strong> - If you can't find what you're looking for or have a specific training requirement, use this link to <a href="mailto:phs.staffdevelopment@phs.scot?Subject=DSKB - Training Request">email Staff Development.</a></p>
+                    <p class="my-4"><strong>Training Request</strong> - If you can't find what you're looking for or have a specific training requirement, use this link to <a href="mailto:phs.staffdevelopment@phs.scot?Subject=DSKB - Training Request">email Staff Development.</a></p>
 
-                <p class="my-4"><strong>Knowledge Base Enquiry</strong> - Feedback, issues, or suggestions for the knowledge base? <a href="https://github.com/Public-Health-Scotland/knowledge-base/issues/new/choose" target="_blank">Open an issue</a> on the GitHub repo or <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Enquiry">email the Data Science team.</a></p>
-              </b-modal>
-            </b-navbar-nav>
-          </b-collapse>
+                    <p class="my-4"><strong>Knowledge Base Enquiry</strong> - Feedback, issues, or suggestions for the knowledge base? <a href="https://github.com/Public-Health-Scotland/knowledge-base/issues/new/choose" target="_blank">Open an issue</a> on the GitHub repo or <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Enquiry">email the Data Science team.</a></p>
+                  </b-modal>
+                </b-navbar-nav>
+              </b-collapse>
+            </div>
+          </b-navbar>
         </div>
-      </b-navbar>
-    </div>
 
-  <div class="container body">
-    <Nuxt />
-  </div>
-
-  <div class="container">
-    <footer class="footer">
-      <div class="copyright-container">
-
-        <b-nav>
-          <b-nav-item v-b-modal.accessibility-modal>Accessibility</b-nav-item>
-          <b-nav-item href="https://www.publichealthscotland.scot/contact-us/freedom-of-information-foi-and-environmental-information-regulation-eir-requests/" target="_blank">Freedom of Information</b-nav-item>
-          <b-nav-item href="https://www.publichealthscotland.scot/terms-and-conditions/" target="_blank">Terms and Conditions</b-nav-item>
-          <b-nav-item href="https://www.publichealthscotland.scot/our-privacy-notice/organisational-background/" target="_blank">Privacy notice</b-nav-item>
-          <b-nav-item disabled>© Public Health Scotland</b-nav-item>
-        </b-nav>
-
-        <b-modal id="accessibility-modal" size="lg" scrollable title="Accessibility">
-          <h5>Accessible Content</h5>
-          This website is run by the Data Science department in Public Health Scotland. We want as many people as possible to be able to use this website. For example, that means you should be able to:
-
-          <ul>
-            <li>change colours, contrast levels and fonts</li>
-            <li>zoom in up to 300% without the text spilling off the screen</li>
-            <li>navigate most of the website using just a keyboard</li>
-            <li>navigate most of the website using speech recognition software</li>
-            <li>listen to most of the website using a screen reader (including the most recent versions of JAWS, NVDA and VoiceOver)</li>
-          </ul>
-
-          We’ve also made the website text as simple as possible to understand.
-
-          We're always looking to improve the accessibility of this website. If you find problems not listed on this page or think we're not meeting accessibility requirements, please email the <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Accessibility">Data Science team.</a></p>
-
-          <h5>Testing</h5>
-
-          <i>This website is currently undergoing development and testing is not yet been complete.</i>
-
-        </b-modal>
-
+      <div class="container body">
+        <Nuxt />
       </div>
-    </footer>
-  </div>
+
+      <div class="container d-flex justify-content-between align-items-center">
+        <div>
+          <footer>
+            <b-nav>
+              <b-nav-item v-b-modal.accessibility-modal>Accessibility</b-nav-item>
+              <b-nav-item href="https://www.publichealthscotland.scot/contact-us/freedom-of-information-foi-and-environmental-information-regulation-eir-requests/" target="_blank">Freedom of Information</b-nav-item>
+              <b-nav-item href="https://www.publichealthscotland.scot/terms-and-conditions/" target="_blank">Terms and Conditions</b-nav-item>
+              <b-nav-item href="https://www.publichealthscotland.scot/our-privacy-notice/organisational-background/" target="_blank">Privacy notice</b-nav-item>
+              <b-nav-item disabled>© Public Health Scotland</b-nav-item>
+            </b-nav>
+          </footer>
+        </div>
+        <div class="">
+          <b-button v-b-modal.feedback-modal class="feedback-button" pill><eva-icon name="message-square-outline" fill="#fff"></eva-icon></b-button>
+        </div>
+      </div>
+
+      <b-modal id="accessibility-modal" size="lg" scrollable title="Accessibility">
+        <h5>Accessible Content</h5>
+        This website is run by the Data Science department in Public Health Scotland. We want as many people as possible to be able to use this website. For example, that means you should be able to:
+
+        <ul>
+          <li>change colours, contrast levels and fonts</li>
+          <li>zoom in up to 300% without the text spilling off the screen</li>
+          <li>navigate most of the website using just a keyboard</li>
+          <li>navigate most of the website using speech recognition software</li>
+          <li>listen to most of the website using a screen reader (including the most recent versions of JAWS, NVDA and VoiceOver)</li>
+        </ul>
+
+        We’ve also made the website text as simple as possible to understand.
+
+        We're always looking to improve the accessibility of this website. If you find problems not listed on this page or think we're not meeting accessibility requirements, please email the <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Accessibility">Data Science team.</a></p>
+
+        <h5>Testing</h5>
+
+        <i>This website is currently undergoing development and testing is not yet been complete.</i>
+
+      </b-modal>
+
+      <b-modal id="feedback-modal" size="xl">
+        If this content doesn't load, <a target="_blank" href="https://forms.office.com/r/RhjP2yNq4y">click here</a> to open in a browser tab.
+        </br>
+        <div>
+          <iframe width="640px" height= "1100px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=veDvEDCgykuAnLXmdF5JmlMENbvENPtAs5eceNN9kYdUQ0JJWlY4UjNOQ1ZCSVlEOUpTSEo3N0JaWS4u&embed=true" frameborder= "0" marginwidth= "0" marginheight= "0" style= "border: none; width:100%" allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> </iframe>
+        </div>
+      </b-modal>
+
   </div>
 </template>
 
@@ -129,7 +139,7 @@ html {
 }
 
 .body {
-  margin-bottom: 150px;
+  margin-bottom: 100px;
 }
 
 *,
@@ -179,10 +189,19 @@ img.logo{
   font-weight: bold;
 }
 
-.footer{
-  position: absolute;
-  bottom: 0;
-  height: 50px;
+.feedback-button{
+  background-color: #3f3685;
+  border-color: #3f3685;
+}
+
+.feedback-button:hover{
+  background-color: #2f2863;
+  border-color: #2f2863;
+}
+
+.feedback-button:focus{
+  background-color: #3f3685;
+  border-color: #3f3685;
 }
 
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-
     <div class="title-container">
       <b-card no-body class="overflow-hidden head-card">
         <b-row no-gutters>
@@ -8,10 +7,21 @@
             <b-card-body>
               <h3>Hey, you ready to learn?</h3>
               <b-card-text>
-                Welcome to the PHS Data Science Knowledge Base. It is here that you'll find access to all resources related to Data Science (R, Python, git, etc.) created by and for PHS staff. This includes in-person and online training, guidance documents, and internally developed textbooks.
+                Welcome to the PHS Data Science Knowledge Base. It is here that
+                you'll find access to all resources related to Data Science (R,
+                Python, git, etc.) created by and for PHS staff. This includes
+                in-person and online training, guidance documents, and
+                internally developed textbooks.
               </b-card-text>
               <b-card-text>
-                <strong>Data Science</strong> is used in the broadest possible sense, capturing introductory content to technologies, and pulling existing and creating new more advanced content from the topic experts across the organisation. If you have something to offer, get in touch by <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Enquiry">emailing the Data Science team.</a>
+                <strong>Data Science</strong> is used in the broadest possible
+                sense, capturing introductory content to technologies, and
+                pulling existing and creating new more advanced content from the
+                topic experts across the organisation. If you have something to
+                offer, get in touch by
+                <a href="mailto:phs.datascience@phs.scot?Subject=DSKB - Enquiry"
+                  >emailing the Data Science team.</a
+                >
               </b-card-text>
             </b-card-body>
           </b-col>
@@ -22,52 +32,78 @@
     <div>
       <b-row cols="1" cols-sm="2" cols-lg="4" class="text-center">
         <b-col>
-          <NuxtLink :to="{path:'/develop', query: {type: ['In-Person Course']}}" ><lord-icon
-            src="https://cdn.lordicon.com/zpxybbhl.json"
-            trigger="hover"
-            colors="primary:#3f3685,secondary:#80ba27"
-            stroke=25
-            style="width:200px;height:200px"
-            title="In-person course icon">
-          </lord-icon></NuxtLink>
+          <NuxtLink
+            :to="{ path: '/develop', query: { type: ['In-Person Course'] } }"
+            ><lord-icon
+              src="https://cdn.lordicon.com/zpxybbhl.json"
+              trigger="hover"
+              colors="primary:#3f3685,secondary:#80ba27"
+              stroke="25"
+              style="width:200px;height:200px"
+              title="In-person course icon"
+            >
+            </lord-icon
+          ></NuxtLink>
           <h5>In-Person Courses</h5>
-          <p>Internally developed courses ran by the PHS staff who use the tools in their day-to-day jobs.</p>
+          <p>
+            Internally developed courses ran by the PHS staff who use the tools
+            in their day-to-day jobs.
+          </p>
         </b-col>
         <b-col>
-          <NuxtLink :to="{path:'/develop', query: {type: ['Online Course']}}" ><lord-icon
-            src="https://cdn.lordicon.com/qhgmphtg.json"
-            trigger="hover"
-            colors="primary:#3f3685,secondary:#80ba27"
-            stroke=25
-            style="width:200px;height:200px"
-            title="Online course icon">
-          </lord-icon></NuxtLink>
+          <NuxtLink
+            :to="{ path: '/develop', query: { type: ['Online Course'] } }"
+            ><lord-icon
+              src="https://cdn.lordicon.com/qhgmphtg.json"
+              trigger="hover"
+              colors="primary:#3f3685,secondary:#80ba27"
+              stroke="25"
+              style="width:200px;height:200px"
+              title="Online course icon"
+            >
+            </lord-icon
+          ></NuxtLink>
           <h5>Online Courses</h5>
-          <p>The same courses available online in addition to or instead of the in-person options.</p>
+          <p>
+            The same courses available online in addition to or instead of the
+            in-person options.
+          </p>
         </b-col>
         <b-col>
-          <NuxtLink :to="{path:'/develop', query: {type: ['Guidance']}}" ><lord-icon
-            src="https://cdn.lordicon.com/nocovwne.json"
-            trigger="hover"
-            colors="primary:#3f3685,secondary:#80ba27"
-            stroke=25
-            style="width:200px;height:200px"
-            title="Guidance document icon">
-          </lord-icon></NuxtLink>
+          <NuxtLink :to="{ path: '/develop', query: { type: ['Guidance'] } }"
+            ><lord-icon
+              src="https://cdn.lordicon.com/nocovwne.json"
+              trigger="hover"
+              colors="primary:#3f3685,secondary:#80ba27"
+              stroke="25"
+              style="width:200px;height:200px"
+              title="Guidance document icon"
+            >
+            </lord-icon
+          ></NuxtLink>
           <h5>Guidance</h5>
-          <p>Keeping everyone on the same page, sharing common knowledge. This includes style guides, FAQs, etc.</p>
+          <p>
+            Keeping everyone on the same page, sharing common knowledge. This
+            includes style guides, FAQs, etc.
+          </p>
         </b-col>
         <b-col>
-          <NuxtLink :to="{path:'/develop', query: {type: ['Textbook']}}" ><lord-icon
-            src="https://cdn.lordicon.com/wxnxiano.json"
-            trigger="morph-two-way"
-            colors="primary:#3f3685,secondary:#80ba27"
-            stroke=25
-            style="width:200px;height:200px"
-            title="Textbook icon">
-          </lord-icon></NuxtLink>
+          <NuxtLink :to="{ path: '/develop', query: { type: ['Textbook'] } }"
+            ><lord-icon
+              src="https://cdn.lordicon.com/wxnxiano.json"
+              trigger="morph-two-way"
+              colors="primary:#3f3685,secondary:#80ba27"
+              stroke="25"
+              style="width:200px;height:200px"
+              title="Textbook icon"
+            >
+            </lord-icon
+          ></NuxtLink>
           <h5>Textbooks</h5>
-          <p>For learning or reference, see a complete textbook on one of the technologies.</p>
+          <p>
+            For learning or reference, see a complete textbook on one of the
+            technologies.
+          </p>
         </b-col>
       </b-row>
     </div>
@@ -75,178 +111,193 @@
     <div class="mt-5">
       <b-row justify-content-between>
         <b-col>
-          <b-card class="mb-4"
-          title="Learning Pathways">
+          <b-card class="mb-4" title="Learning Pathways">
             <div class="d-flex mb-4">
-              <b-card-text>Sometimes it's easier to get started when you can plan for the road ahead...</b-card-text>
+              <b-card-text
+                >Sometimes it's easier to get started when you can plan for the
+                road ahead...</b-card-text
+              >
             </div>
             <b-row class="mb-3">
               <b-col>
-                <b-button block variant="outline-primary" class="mt-2" @click="$router.push('/develop/r-pathway')">R Pathway</b-button>
+                <b-button
+                  block
+                  variant="outline-primary"
+                  class="mt-2"
+                  @click="$router.push('/develop/r-pathway')"
+                  >R Pathway</b-button
+                >
               </b-col>
               <b-col cols="12" md="10">
-                <b-card-text>The R content has been desgined to be topic-led, letting you pick up the concepts and tools for when you need them. However, the R pathway page outlines a structure for all of the R content. This includes training modules, guidance, and related technologies.</b-card-text>
+                <b-card-text
+                  >The R content has been desgined to be topic-led, letting you
+                  pick up the concepts and tools for when you need them.
+                  However, the R pathway page outlines a structure for all of
+                  the R content. This includes training modules, guidance, and
+                  related technologies.</b-card-text
+                >
               </b-col>
             </b-row>
           </b-card>
         </b-col>
       </b-row>
     </div>
-
   </div>
 </template>
 
 <script>
-import Multiselect from 'vue-multiselect'
-import VueMarkdown from 'vue-markdown'
-import courses from '@/assets/courses.json'
+import Multiselect from "vue-multiselect";
+import VueMarkdown from "vue-markdown";
+import courses from "@/assets/courses.json";
 
 export default {
   components: { Multiselect, VueMarkdown },
-  data(){
+  data() {
     return {
       courses,
       selectedTags: [],
       selectedTypes: [],
       selectedCourse: {},
-      search: '',
-      filterSelected: ["Online Course", "In-Person Course", "Textbook", "Guidance"]
-    }
+      search: "",
+      filterSelected: [
+        "Online Course",
+        "In-Person Course",
+        "Textbook",
+        "Guidance"
+      ]
+    };
   },
-  computed:{
-    tags(){
-      let tags = []
-      this.courses.forEach(course => tags = tags.concat(course.tags))
-      return [...new Set(tags)]
+  computed: {
+    tags() {
+      let tags = [];
+      this.courses.forEach(course => (tags = tags.concat(course.tags)));
+      return [...new Set(tags)];
     },
-    types(){
-      let types = []
-      this.courses.forEach(course => types = types.concat(course.type))
-      return [...new Set(types)]
+    types() {
+      let types = [];
+      this.courses.forEach(course => (types = types.concat(course.type)));
+      return [...new Set(types)];
     },
 
-    displayCourses(){
+    displayCourses() {
       // Add all the courses that have at least 1 matching tags
       // to the list of selectedTags
       //
-      let courses = this.courses
+      let courses = this.courses;
 
-      if(!this.filterSelected.includes("Coming Soon")) {
-        courses = this.courses.filter(course =>
-          course.link != "")
+      if (!this.filterSelected.includes("Coming Soon")) {
+        courses = this.courses.filter(course => course.link != "");
       }
 
-      if(this.search.length > 0 && this.search.length <= 1){
-        courses = courses.filter( course => {
-          if(course.tags.includes(this.search.toLowerCase())) return true
-          else return false
-        })
-      } else if (this.search.length > 1){
-        courses = courses.filter( course => {
-          if(course.title.toLowerCase().includes(this.search.toLowerCase())) return true
-          else if(course.description.toLowerCase().includes(this.search.toLowerCase())) return true
-          else return false
-        })
+      if (this.search.length > 0 && this.search.length <= 1) {
+        courses = courses.filter(course => {
+          if (course.tags.includes(this.search.toLowerCase())) return true;
+          else return false;
+        });
+      } else if (this.search.length > 1) {
+        courses = courses.filter(course => {
+          if (course.title.toLowerCase().includes(this.search.toLowerCase()))
+            return true;
+          else if (
+            course.description.toLowerCase().includes(this.search.toLowerCase())
+          )
+            return true;
+          else return false;
+        });
       }
 
-      courses = courses.filter( course => {
-        if(this.filterSelected.includes(course.type)) return true
-        else return false
-      })
+      courses = courses.filter(course => {
+        if (this.filterSelected.includes(course.type)) return true;
+        else return false;
+      });
 
-      return courses
+      return courses;
     }
   },
 
-
-
-  methods:{
-    async showGuidance(course){
-      course.md = await this.$axios.$get('https://secret-ocean-49799.herokuapp.com/'+course.link)
-      this.selectedCourse=course
+  methods: {
+    async showGuidance(course) {
+      course.md = await this.$axios.$get(
+        "https://secret-ocean-49799.herokuapp.com/" + course.link
+      );
+      this.selectedCourse = course;
     }
   }
-
-
-
-}
+};
 </script>
 
 <style>
-  .filter-row{
-    display: flex;
-    align-items: center;
-  }
+.filter-row {
+  display: flex;
+  align-items: center;
+}
 
-  input{
-    min-width: 120px;
-    flex-grow: 1
-  }
+input {
+  min-width: 120px;
+  flex-grow: 1;
+}
 
-  .filter{
-    flex: 0 1 auto;
-  }
+.filter {
+  flex: 0 1 auto;
+}
 
-  .title-container{
-    margin-bottom: 20px;
-  }
+.title-container {
+  margin-bottom: 20px;
+}
 
-  .head-card{
-    border: none;
-  }
+.head-card {
+  border: none;
+}
 
-  .tag-cloud{
-    padding: 0 10px;
-  }
+.tag-cloud {
+  padding: 0 10px;
+}
 
-  .tag-button{
-    margin: 2px 0;
-  }
+.tag-button {
+  margin: 2px 0;
+}
 
-  b-button{
-    margin-top: red;
-  }
+b-button {
+  margin-top: red;
+}
 
-  b-dropdown{
-    margin-right: 15px;
-  }
+b-dropdown {
+  margin-right: 15px;
+}
 
-  .grid{
-    display: grid;
-  }
+.grid {
+  display: grid;
+}
 
-  .multiselect__option--highlight, .multiselect__option--highlight::after{
-    background: #80BA27 !important;
-  }
+.multiselect__option--highlight,
+.multiselect__option--highlight::after {
+  background: #80ba27 !important;
+}
 
-  .multiselect__option--selected.multiselect__option--highlight, .multiselect__option--selected.multiselect__option--highlight::after{
-    background: #bd27b9 !important;
-  }
+.multiselect__option--selected.multiselect__option--highlight,
+.multiselect__option--selected.multiselect__option--highlight::after {
+  background: #bd27b9 !important;
+}
 
-  .multiselect__tag{
-    background: #80BA27 !important;
-  }
+.multiselect__tag {
+  background: #80ba27 !important;
+}
 
-  .btn-primary{
-    background-color: #0078d4;
-    float: right;
-  }
+.btn-primary {
+  background-color: #0078d4;
+  float: right;
+}
 
-  .coming-soon{
-    color: #0078d4;
-    float: right;
-  }
-  .guidance-md {
-    padding: 0 20px;
-  }
-  .guidance-md img{
-    width: 100%;
-  }
-
-  footer{
-    margin-top: 50px;
-    margin-bottom: 50px;
-  }
+.coming-soon {
+  color: #0078d4;
+  float: right;
+}
+.guidance-md {
+  padding: 0 20px;
+}
+.guidance-md img {
+  width: 100%;
+}
 </style>
 
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
