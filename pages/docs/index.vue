ce8dc3f97dec05ff97459ae0d6e5050c9651a5d4
rmccreath <mccreathr@gmail.com>
2023-02-01 13:37:27 +0000

    Add method to retrieve directory structure

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -4,8 +4,6 @@
     <p>Some docs</p>
     <pre>{{ login }}</pre>
     
-
-
       <div>
         <b-nav vertical class="w-25">
           <b-nav-item v-for="item in docs" :key="item.name" :to="item.path">
@@ -27,6 +25,9 @@ export default {
       docs: []
     }
   },
+
+  
+
   async created(){
     let docs = await this.$axios.get('/repos/Public-Health-Scotland/technical-docs/contents/',{
       baseURL: 'https://api.github.com'
@@ -35,8 +36,26 @@ export default {
      docs = docs.filter(d => !d.name.startsWith('.'))
      docs = docs.filter(d => !d.name.startsWith('README'))
 
-    this.docs = docs
+     let getSubDocs = async (docs) => {
+      for(let i = 0; i < docs.length; i++){
+        let doc = docs[i]
+        if(doc.type == 'dir'){
+          let subDocs = await this.$axios.get('/repos/Public-Health-Scotland/technical-docs/contents/' + doc.path,{
+            baseURL: 'https://api.github.com'
+          }).then(r => r.data)
+          subDocs = subDocs.filter(d => !d.name.startsWith('.'))
+          subDocs = subDocs.filter(d => !d.name.startsWith('README'))
+          doc.children = subDocs
+          getSubDocs(subDocs)
+        }
+      }
+
+      return docs
+    }
+
+    this.docs = await getSubDocs(docs)
   }
+
 }
 
 </script>
