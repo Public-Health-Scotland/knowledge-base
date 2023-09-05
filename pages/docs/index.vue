725657d6ba2473a7655c2b0d88b58986cfc8e503
rmccreath <russell.mccreath@phs.scot>
2023-09-05 14:11:55 +0100

    Fix packages and add sessions link to docs home

--- pages/docs/index.vue
+++ pages/docs/index.vue
@@ -1,26 +1,56 @@
 <template>
-    <div>
-        <b-row>
-            <b-col>
-                <h1>Data Science - the docs</h1>
-                <p>This page pulls together documentation relating to Data Science in Public Health Scotland. This is organised into sections as shown in the navigation on the left-side of this page. You can also use the search bar above which will check document titles to help you find what you're looking for. This is all stored and organised on a GitHub repo, each document has a button to link back, the aim of this is to encourage collaboration on our documentation, allowing us to work as a community to maintain our technical knowledge and guidance.</p>
-            </b-col>
-        </b-row>
-        <b-row class="mt-5">
-            <b-col>
-                <h3>Get Started</h3>
-                <ul>
-                    <li>
-                        <strong><NuxtLink to="/docs/Posit%20Infrastructure">What is Posit?</NuxtLink></strong>
-                    </li>
-                    <li>
-                        <strong><NuxtLink to="/docs/Posit%20Infrastructure?doc=Posit%20Support.md">Getting support with Posit</NuxtLink></strong>
-                    </li>
-                    <li>
-                        <strong><NuxtLink to="/docs/Posit%20Infrastructure?doc=FAQs.md">Posit Frequently Asked Questions (FAQs)</NuxtLink></strong>
-                    </li>
-                </ul>
-            </b-col>
-        </b-row>
-    </div>
+  <div>
+    <b-row>
+      <b-col>
+        <h1>Data Science - the docs</h1>
+        <p>
+          This page pulls together documentation relating to Data Science in
+          Public Health Scotland. This is organised into sections as shown in
+          the navigation on the left-side of this page. You can also use the
+          search bar above which will check document titles to help you find
+          what you're looking for. This is all stored and organised on a GitHub
+          repo, each document has a button to link back, the aim of this is to
+          encourage collaboration on our documentation, allowing us to work as a
+          community to maintain our technical knowledge and guidance.
+        </p>
+      </b-col>
+    </b-row>
+    <b-row class="mt-5">
+      <b-col>
+        <h3>Get Started</h3>
+        <ul>
+          <li>
+            <strong
+              ><NuxtLink to="/docs/Posit%20Infrastructure"
+                >What is Posit?</NuxtLink
+              ></strong
+            >
+          </li>
+          <li>
+            <strong
+              ><NuxtLink
+                to="/docs/Quick%20guide%20to%20your%20Posit%20Workbench%20session.md"
+                >Quick guide to Posit Workbench sessions</NuxtLink
+              ></strong
+            >
+          </li>
+          <li>
+            <strong
+              ><NuxtLink
+                to="/docs/Posit%20Infrastructure?doc=Posit%20Support.md"
+                >Getting support with Posit</NuxtLink
+              ></strong
+            >
+          </li>
+          <li>
+            <strong
+              ><NuxtLink to="/docs/Posit%20Infrastructure?doc=FAQs.md"
+                >Posit Frequently Asked Questions (FAQs)</NuxtLink
+              ></strong
+            >
+          </li>
+        </ul>
+      </b-col>
+    </b-row>
+  </div>
 </template>
